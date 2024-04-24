614c727b914a25ed5ea431037604e4cc21b6ee79
ritikkashyap720 <72151729+ritikkashyap720@users.noreply.github.com>
2024-04-24 13:11:24 +0530

    Delete backend directory

--- backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {handleCompilation,handleCheckStatus,handleDownload} = require("../controllers/compiler");
-const router = express.Router();
-
-router.post("/",handleCompilation)
-router.get("/status/:jobId",handleCheckStatus)
-router.post("/download",handleDownload)
-
-module.exports = router
